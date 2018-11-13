e0a0d4a2487edb39bd1781694e26f776623889b0
Srujana Gattu <S533622@NWMISSOURI.EDU>
2018-11-12 23:07:03 -0600

    Created customer.js in models folder Created customer.json in data folder Updated index.ejs Updated css file

---
+++ models/customer.js
@@ -0,0 +1,78 @@
+/** 
+*  Customer model
+*  Describes the characteristics of each attribute in a customer resource.
+*
+* @author Srujana Gattu <S533622@nwmissouri.edu>
+*
+*/
+
+// bring in mongoose 
+// see <https://mongoosejs.com/> for more information
+const mongoose = require('mongoose')
+
+const CustomerSchema = new mongoose.Schema({
+
+    _id: {
+        type: number,
+        required: true,
+        description: "The unique identifier to identify a Customer"
+    },
+    
+    firstname: {
+        type: String,
+        required: true,
+        default: 'First Name'
+       
+    },
+    lastname: {
+        type: String,
+        required: true,
+        default: 'Last Name'
+       
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    age: {
+        type: number,
+        required: true
+       
+    },
+    street1: {
+        type: String,
+        required: true,
+        default: 'Street 1'
+    },
+    street2: {
+        type: String,
+        required: false,
+        default: 'Street 2'
+    },
+    city: {
+        type: String,
+        required: true,
+        default: 'Maryville'
+    },
+    state: {
+        type: String,
+        required: true,
+        default: 'MO'
+    },
+    country: {
+        type: String,
+        required: true,
+        default: 'USA'
+    },
+    zip: {
+        type: String,
+        required: true,
+        default: '64468'
+    }
+
+
+
+
+})
+module.exports = mongoose.model('Customer', CustomerSchema)
